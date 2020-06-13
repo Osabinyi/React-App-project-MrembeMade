8e8a93e93091f5c941f8edd0088b99fe92bd4b9c
Osabinyi <47927307+Osabinyi@users.noreply.github.com>
2020-06-13 19:13:03 +0300

    Add files via upload

---
+++ src/components/pages/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function About() {
+   return (
+      <React.Fragment>
+       <h1>About</h1>
+       <p>This is the TododList app v1.0.0. it is created by</p>
+       </React.Fragment>
+   )
+}
