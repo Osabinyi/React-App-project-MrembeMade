ab0239bba285a14168dfa8e7de8502997ac604b7
Osabinyi <47927307+Osabinyi@users.noreply.github.com>
2020-06-13 19:11:13 +0300

    Add files via upload

---
+++ build/precache-manifest.d872a7aaa811cc20ba0b54e6d0a24a38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d498de235e7fd421bfcada8675690a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cdb50851115e915133ad",
+    "url": "/static/css/main.30a89d6e.chunk.css"
+  },
+  {
+    "revision": "c5f31209e36c34434acf",
+    "url": "/static/js/2.fb0f36da.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.fb0f36da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdb50851115e915133ad",
+    "url": "/static/js/main.0c9991da.chunk.js"
+  },
+  {
+    "revision": "3cb015972f7e045032d6",
+    "url": "/static/js/runtime-main.f1bc8154.js"
+  }
+]);
